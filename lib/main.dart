31f7c973764c50d32212f1f68096f045e14db144
Recep Sevim <recep_sevim20@hotmail.com>
2022-06-13 22:49:14 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_app/data/local_storage.dart';
+import 'package:todo_app/screens/home_page.dart';
+
+import 'models/task_model.dart';
+
+final locator = GetIt.instance;
+
+void setup() {
+  locator.registerSingleton<LocalStorage>(HiveLocalStorage());
+}
+
+Future<void> setupHive() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  var taskBox = await Hive.openBox<Task>('tasks');
+  for (var task in taskBox.values) {
+    if (task.createdAt.day != DateTime.now().day) {
+      taskBox.delete(task.id);
+    }
+  }
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  await setupHive();
+
+  setup();
+  runApp(const MyApp());
+}
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              iconTheme: IconThemeData(color: Colors.black))),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:todo_app/main.dart';
+// import 'package:flutter_todo_app/data/local_storage.dart';
+// import 'package:flutter_todo_app/helper/translation_helper.dart';
+// import 'package:flutter_todo_app/main.dart';
+// import 'package:flutter_todo_app/models/task_model.dart';
+// import 'package:flutter_todo_app/widgets/custom_search_Delegate.dart';
+// import 'package:flutter_todo_app/widgets/task_list_item.dart';
+
+import '../data/local_storage.dart';
+import '../models/task_model.dart';
+import '../widgets/custom_search_delegate.dart';
+import '../widgets/task_list_item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Task> _allTasks;
+  late LocalStorage _localStorage;
+
+  @override
+  void initState() {
+    super.initState();
+    _localStorage = locator<LocalStorage>();
+    _allTasks = <Task>[];
+    _getAllTaskFromDb();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: GestureDetector(
+          onTap: () {
+            _showAddTaskBottomSheet();
+          },
+          child: const Text(
+            'Yapılacaklar Listesi',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              _showSearchPage();
+            },
+            icon: const Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              _showAddTaskBottomSheet();
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: _allTasks.isNotEmpty
+          ? ListView.builder(
+              itemBuilder: (context, index) {
+                var _oankiListeElemani = _allTasks[index];
+                return Dismissible(
+                  background: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.delete,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Görevi Sil')
+                    ],
+                  ),
+                  key: Key(_oankiListeElemani.id),
+                  onDismissed: (direction) {
+                    _allTasks.removeAt(index);
+                    _localStorage.deleteTask(task: _oankiListeElemani);
+                    setState(() {});
+                  },
+                  child: TaskItem(task: _oankiListeElemani),
+                );
+              },
+              itemCount: _allTasks.length,
+            )
+          : const Center(
+              child: Text('Yeni görev ekle.'),
+            ),
+    );
+  }
+
+  void _showAddTaskBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          width: MediaQuery.of(context).size.width,
+          child: ListTile(
+            title: TextField(
+              autofocus: true,
+              style: const TextStyle(fontSize: 20),
+              decoration: const InputDecoration(
+                hintText: 'Görev ekle.',
+                border: InputBorder.none,
+              ),
+              onSubmitted: (value) {
+                Navigator.of(context).pop();
+                if (value.length > 3) {
+                  DatePicker.showTimePicker(context, showSecondsColumn: false,
+                      onConfirm: (time) async {
+                    var yeniEklenecekGorev =
+                        Task.create(name: value, createdAt: time);
+
+                    _allTasks.insert(0, yeniEklenecekGorev);
+                    await _localStorage.addTask(task: yeniEklenecekGorev);
+                    setState(() {});
+                  });
+                }
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _getAllTaskFromDb() async {
+    _allTasks = await _localStorage.getAllTask();
+    setState(() {});
+  }
+
+  void _showSearchPage() async {
+    await showSearch(
+        context: context, delegate: CustomSearchDelegate(allTasks: _allTasks));
+    _getAllTaskFromDb();
+  }
+}
