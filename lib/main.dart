5762be70029c82fe18a93731ee30cf6051a97a8c
Recep Sevim <recep_sevim20@hotmail.com>
2022-06-17 18:35:08 +0300

    published

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ToDo App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
